0aab17deb3d298bbc23acb003fbe30e359564d76
Victor <gamerguyxxx123@gmail.com>
2024-03-13 13:08:54 +0100

    noget af login
    kun det visuelle så man kan logge ind men uden database

--- Scripts/login_menu.gd
+++ Scripts/login_menu.gd
@@ -10,6 +10,18 @@ var created = false
 func _on_login_button_down():
 	if !created:
 		username = $Username.text
-		password = $Password.text.sha256_text
+		password = $Password.text.sha256_text()
 		created = true
+		$Login.text = "Login"
+		$Username.text = ""
+		$Password.text = ""
 		print("Account created")
+	else:
+		if $Username.text == username:
+			if $Password.text.sha256_text() == password:
+				print("Login Success!")
+				get_tree().change_scene_to_file("res://Scenes/Screens/menu.tscn")
+			else:
+				print("Login failed!")
+		else:
+				print("Login failed again!")
