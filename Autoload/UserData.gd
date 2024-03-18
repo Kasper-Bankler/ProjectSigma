b9c1f767b81f0812c7871ad4cb2bc029e7703e09
Kasper Bankler <kasperbankler@gmail.com>
2024-03-18 14:16:25 +0100

    Create UserData script (AutoLoad)

---
+++ Autoload/UserData.gd
@@ -0,0 +1,17 @@
+extends Node
+
+signal user_updated
+signal progression_cleared
+
+var username
+var score = []
+
+func update_user(value: int) -> void:
+	score.append(value)
+	#Update database
+	emit_signal("user_updated")
+	
+func clear_progression() -> void:
+	score.clear()
+	# Clear user progression in database
+	emit_signal("progression_cleared")
