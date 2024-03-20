facc4b6fb211d4d7bdf6a1e6c0bf8d51496e17eb
Victor <gamerguyxxx123@gmail.com>
2024-03-20 14:12:26 +0100

    SDSDF

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -4,11 +4,11 @@ extends Control
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Main_game.tscn")
 
 
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://options.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Screens/options.tscn")
 
 
 func _on_quit_pressed():
--- Scripts/options.gd
+++ Scripts/options.gd
@@ -5,4 +5,4 @@ extends Control
 
 
 func _on_back_pressed():
-	get_tree().change_scene_to_file("res://menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Screens/menu.tscn")
