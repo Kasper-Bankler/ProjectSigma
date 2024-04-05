6dc98331c33bd6357f8117c097c735188550c6d5
Christian Walker Petersen <71141167+The-cool-guy@users.noreply.github.com>
2024-04-05 12:56:45 +0200

    ali fiks
    l bozo

--- Scripts/Builder.gd
+++ Scripts/Builder.gd
@@ -1,16 +1,21 @@
-extends TileMap
+extends Node2D
 
+var wind = preload("res://Scenes/Buildings/windturbine.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var tile = local_to_map(get_global_mouse_position())
+	var mouseLocation = get_global_mouse_position()
+
 	if (Input.is_action_just_pressed("ui_accept")):
-		print("det virkede");
-		set_cell(0,tile,0,Vector2(0,0),0)
-
-
+		var tileLocation=(Vector2(UserData.tile_posiiton.x,UserData.tile_posiiton.y))
+		var building=wind.instantiate()
+		$".".add_child(building)
+		print(UserData.tile_posiiton)
+		print(tileLocation.x*130, ",", tileLocation.y*64)
+		
+		building.position.x=tileLocation.x
+		building.position.y=tileLocation.y
--- Scripts/tilemap.gd
+++ Scripts/tilemap.gd
@@ -9,8 +9,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var tile=local_to_map(get_global_mouse_position())
+
+	UserData.tile_posiiton=tile
 	for i in 20:
 		for o in 20:
 			erase_cell(1,Vector2(i,o))
-			
+	
+	
 	set_cell(1,tile,25,Vector2i(0,0),0)
+
+
+
