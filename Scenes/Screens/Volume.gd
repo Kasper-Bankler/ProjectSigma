2a4c358f9a70bdcc9b71e0ad0f34058252af4241
Victor <gamerguyxxx123@gmail.com>
2024-04-15 13:40:01 +0200

    Volume slider virker nu

--- Scenes/Screens/Volume.gd
+++ Scenes/Screens/Volume.gd
@@ -1,12 +1,10 @@
 extends HSlider
 
-@export
-var bus_name: String
-
-var bus_index: int
+var bus_index
 
 func _ready() -> void:
-	bus_index = AudioServer.get_bus_index(bus_name)
+	bus_index = AudioServer.get_bus_index("Master")
+	
 	value_changed.connect(_on_changed)
 	
 	value = db_to_linear(
@@ -14,7 +12,9 @@ func _ready() -> void:
 	)
 
 func _on_changed(value: float) -> void:
+	
 	AudioServer.set_bus_volume_db(
 		bus_index,
 		linear_to_db(value)
 	)
+	
