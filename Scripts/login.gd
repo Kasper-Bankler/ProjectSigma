5e06188b7d7c64fc1c7f20f0cdd83e37bd177c37
Ali <39883081+goorekt@users.noreply.github.com>
2024-04-16 13:41:02 +0200

    asæljkasfælkjadsæo

--- Scripts/login.gd
+++ Scripts/login.gd
@@ -1,16 +1,16 @@
 extends Control
 
-var username = ""
-var password
-
 @onready var usernameField =$Username
 @onready var passwordField =$Password
+
+var username
+var password
 
 func _process(delta):
 	if UserData.is_requesting:
 		$Login.set_text("Loading...")
 	else:
-		$Login.set_text("Sign up")
+		$Login.set_text("Login")
 		
 
 func _on_login_button_down():
@@ -25,10 +25,4 @@ func _on_login_button_down():
 	if !UserData.is_requesting:
 		username = usernameField.get_text()
 		password = passwordField.get_text()
-		UserData.register_new_user(username,password)
-
-
-		$Username.text = ""
-		$Password.text = ""
-	
-		
+		UserData.login_user(username,password)
