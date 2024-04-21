891610faf8692cfee87e92857eb54336df218d07
Ali <39883081+goorekt@users.noreply.github.com>
2024-04-21 22:38:07 +0200

    hashing password in db + cleaning and comments of login.gd, login_menu.gd and Network.gd

--- Scripts/login.gd
+++ Scripts/login.gd
@@ -1,22 +1,29 @@
 extends Control
 
-@onready var usernameField =$VBoxContainer/HBoxContainer2/Username
-@onready var passwordField =$VBoxContainer/HBoxContainer/Password
-
+@onready var usernameField = $VBoxContainer/HBoxContainer2/Username
+@onready var passwordField = $VBoxContainer/HBoxContainer/Password
+@onready var loginButton = $VBoxContainer/Login
 var username
 var password
 
 func _ready():
 	UserData.log_in_response.connect(on_login_response)
+	
+	
 func _process(delta):
+	
+	#Ændrer knappen til Loading hvis i gang med at hente brugerdata fra db
 	if UserData.is_requesting:
-		$VBoxContainer/Login.set_text("Loading...")
+		loginButton.set_text("Loading...")
+		loginButton.disabled=true
 	else:
-		$VBoxContainer/Login.set_text("Login")
-
+		loginButton.set_text("Login")
+		loginButton.disabled=false
 
 func _on_login_pressed():
 
+	#Giver popup hvis et af felterne er tomt
+	
 	if usernameField.get_text()=="":
 		MusicController.errorSound()
 		UserData.popup_message("Husk at vælge et brugernavn!",$".")
@@ -26,24 +33,36 @@ func _on_login_pressed():
 		MusicController.errorSound()
 		UserData.popup_message("Husk at vælge en adgangskode!",$".")
 		return
-		
-	if !UserData.is_requesting:
-		username = usernameField.get_text()
-		password = passwordField.get_text()
-		UserData.login_user(username,password)
+	
+	
+	#Forsøger at finde bruger med den indtastede adgangskode og logind
+	
+	username = usernameField.get_text()
+	password = passwordField.get_text()
+	UserData.login_user(username,password)
 
 func on_login_response(res):
+	
+	#Hvis objectet er tom er det fordi der ikke eksisterede nogen bruger med det indtastede brugernavn
+	
 	if ("username" not in res):
 		MusicController.errorSound()
 		UserData.popup_message("Ingen bruger fundet med indtastede brugernavn!",$".")
 		return
-			
+	
+	#Giver brugeren besked hvis adgangskode ikke er korrekt
+	
 	elif (res["password"]!=res["input_password"]):
 		MusicController.errorSound()
 		UserData.popup_message("Den indtastede adgangskode er forkert!",$".")
 		return
+	
+	#Logger brugeren ind til spillet
+	
 	UserData.logged_in_username=res["username"]
 	get_tree().change_scene_to_file("res://Scenes/Screens/StartMenu.tscn")
+
+#Skifter scene til bruger registreringsside
 
 func _on_login_2_pressed():
 	MusicController.clickSound()
--- Scripts/login_menu.gd
+++ Scripts/login_menu.gd
@@ -3,50 +3,75 @@ extends Control
 var username = ""
 var password
 
-@onready var usernameField =$VBoxContainer/HBoxContainer2/Username
-@onready var passwordField =$VBoxContainer/HBoxContainer/Password
-
+@onready var usernameField = $VBoxContainer/HBoxContainer2/Username
+@onready var passwordField = $VBoxContainer/HBoxContainer/Password
+@onready var signup_button = $VBoxContainer/Login2
 
 func _ready():
 	UserData.connect("signed_up",signed_up)
 
 
 func signed_up(encountered_error):
+	
+	#Kontrolleret error om allerede brugt brugernavn håndteres
 	if encountered_error:
 		UserData.popup_message("Der eksisterer en anden bruger med det valgte brugernavn!",$".")
 		return
+	
+	#Det er lykkes at registrere en ny bruger og brugeren sendes til login side 
 	MusicController.confirmationSound()
 	get_tree().change_scene_to_file("res://Scenes/Screens/login.tscn")
 	
 func _process(delta):
+	
+		#Ændrer knappen til Loading hvis i gang med at hente brugerdata fra db
 	if UserData.is_requesting:
-		$VBoxContainer/Login2.set_text("Loading...")
+		signup_button.set_text("Loading...")
+		signup_button.disabled=true
 	else:
 		$VBoxContainer/Login2.set_text("Sign up")
+		signup_button.disabled=false
 		
 
 func _on_login_button_down():
-	if usernameField.get_text()=="":
+	
+	var input_password = passwordField.get_text()
+	var input_username = usernameField.get_text()
+	
+	#Giver popup hvis et af felterne er tomt
+	
+	if input_username=="":
 		MusicController.errorSound()
 		UserData.popup_message("Husk at vælge et brugernavn!",$".")
 		return
 		
-	if passwordField.get_text()=="":
+	if input_password=="":
 		MusicController.errorSound()
 		UserData.popup_message("Husk at vælge en adgangskode!",$".")
 		return
-	if usernameField.get_text().contains(" ") or not usernameField.get_text().is_valid_filename():
+		
+	#Giver popup besked hvis et af felterne er ugyldigt
+	
+	if input_username.contains(" ") or not input_username.is_valid_filename():
 		MusicController.errorSound()
 		UserData.popup_message("Ugyldigt brugernavn!",$".")
 		return
-	if !UserData.is_requesting:
-		username = usernameField.get_text()
-		password = passwordField.get_text()
-		UserData.register_new_user(username,password)
+	if input_password.contains(" ") or not input_password.is_valid_filename():
+		MusicController.errorSound()
+		UserData.popup_message("Ugyldigt brugernavn!",$".")
+		return
 		
-		usernameField.text = ""
-		passwordField.text = ""
+		
+	#Forsøger at registrerw brugeren i db
+	
+	UserData.register_new_user(input_username,input_password)
+	
+	#Tømmer begge input felter
+	
+	usernameField.text = ""
+	passwordField.text = ""
 
+#Skifter scene til login siden
 
 func _on_login_2_pressed():
 	MusicController.clickSound()
