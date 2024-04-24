e56b0104284d512201f16ba02a836d8b6347744b
Ali <39883081+goorekt@users.noreply.github.com>
2024-04-24 22:23:52 +0200

    remove button

--- Scripts/login_menu.gd
+++ Scripts/login_menu.gd
@@ -78,7 +78,3 @@ func _on_login_button_down():
 func _on_login_2_pressed():
 	MusicController.clickSound()
 	get_tree().change_scene_to_file("res://Scenes/Screens/login.tscn")
-
-
-func _on_scene__change_2_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Screens/StartMenu.tscn")
